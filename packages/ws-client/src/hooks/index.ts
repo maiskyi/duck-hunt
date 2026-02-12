c7576ef23b4aa2665e1257bdcbfa8a0ca92ff23f
Andrii Maiskyi <andrey@wearehelful.com>
2026-02-12 17:00:28 +0000

    feat: client. #1

---
+++ packages/ws-client/src/hooks/index.ts
@@ -0,0 +1,3 @@
+'use client';
+
+export { useSocket, useSocketEmit, useSocketEvent } from "use-socket-io-react";
--- packages/ws-client/src/index.ts
+++ packages/ws-client/src/index.ts
@@ -1,2 +1,12 @@
 // Components
-export * from './components/WsClientProvider';
+export * from "./components/WsClientProvider";
+
+// Hooks
+export * from "./hooks";
+
+// Types
+export type {
+  EventHandler,
+  ServerToClientEvents,
+  ClientToServerEvents,
+} from "use-socket-io-react";
