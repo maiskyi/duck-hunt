0bb0a8e146fd330313130802b7189a81e0e51ac0
Andrii Maiskyi <andrey@wearehelful.com>
2026-02-17 13:54:26 +0000

    feat: game. #1

--- apps/api/src/duck-hunt/services/duckHunt/duckHunt.const.ts
+++ apps/api/src/duck-hunt/services/duckHunt/duckHunt.const.ts
@@ -1,7 +1,5 @@
-export const NEXT_ROUND_MIN_DELAY_MS = 5000;
+export const NEXT_ROUND_MIN_DELAY_MS = 10000;
 
-export const NEXT_ROUND_MAX_DELAY_MS = 10000;
+export const NEXT_ROUND_MAX_DELAY_MS = 30000;
 
-export const FLIGHT_DURATION_MS = 20000;
-
-export const SHOW_AFTER_HIT_MS = 3000;
+export const FLIGHT_DURATION_MS = 5000;
